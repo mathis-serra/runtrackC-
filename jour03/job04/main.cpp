58a546d1a0a8dc458830f08bfc9d36b5aa0b55ea
mathis-serra <mathis.serra@laplateforme.io>
2024-05-02 08:35:00 +0200

    finish job04

---
+++ jour03/job04/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string chaine = "Vive la plateforme ! ";
+
+    cout << "la taille est de " << chaine.size() << endl;
+
+    return 0 ;
+
+}
