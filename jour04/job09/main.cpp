752b63647a9e29ef29910d3a5db4d5a572adcbe5
mathis-serra <mathis.serra@laplateforme.io>
2024-05-03 15:27:35 +0200

    finish job09

---
+++ jour04/job09/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int main()
+{
+    vector <int> vect;
+    int val;
+    int iter = 10;
+    
+
+    for(int i = 0; i < iter ; i++){ 
+        cout << "inserer un nombre :" << endl; 
+        cin >> val;
+        vect.push_back(val);
+    }
+
+    cout << "tableau apres les valeurs ajoutée" << endl;
+    for(int i = 0; i < vect.size() ; i++){
+        cout << vect[i] << endl;
+    }
+
+        
+    return 0;
+}
