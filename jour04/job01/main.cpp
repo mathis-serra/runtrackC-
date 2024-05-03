e40837acce08f20e84383919343b44600d4a5608
mathis-serra <mathis.serra@laplateforme.io>
2024-05-03 10:44:45 +0200

    finish Job01

---
+++ jour04/job01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int number = 2019;
+
+    
+    int *ptr = &number;
+
+    
+    cout << "La valeur de number est : " << *ptr << endl;
+
+    return 0;
+}
